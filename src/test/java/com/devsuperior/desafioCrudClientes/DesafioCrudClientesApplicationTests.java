8373984426bc12e41f78e23f47d1d0027f9891b4
Alexandre Matos <matosalexandre795@gmail.com>
2025-09-03 10:41:38 -0300

    Project Created!

---
+++ src/test/java/com/devsuperior/desafioCrudClientes/DesafioCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioCrudClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
